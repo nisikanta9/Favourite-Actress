2d1242454cd7eac70596b2dfd32ea9b1731d2c76
Nisikanta Sahoo <nisikanta9@gmail.com>
2025-07-13 20:51:58 +0530

    Add files via upload
    To get the favourite actress name as input from the end-user, which is sunny, till the name is sunny it asks for input ( by using html and java script ).

---
+++ actress.js
@@ -0,0 +1,6 @@
+// (To get the favourite actress name as input from the end-user,which is sunny,till the name is sunny it asks for input[By using javascript])
+var name=prompt('Enter your favourite actress name:')
+while(name!=='sunny'){
+  name=prompt('Enter your favourite actress name:')
+}
+alert('Thanks for confirmation as your favourite actress is '+name)
